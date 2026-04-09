036ba57998386801a41d1daf375af1427c16aeaf
CarlosKApaza <kevin14apaza@gmail.com>
2026-04-09 01:17:31 -0300

    estructura corregida

--- Practicas/Practica2 - Soquets RMI/Practica2_Micro_Sistema_Judicial/src/main/java/com/mycompany/practica2_micro_sistema_judicial/RespuestaCuenta.java
+++ Practicas/Practica2 - Soquets RMI/Practica2_Micro_Sistema_Judicial/src/main/java/com/mycompany/practica2_micro_sistema_judicial/RespuestaCuenta.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.practica2;
+package com.mycompany.practica2_micro_sistema_judicial;
 
 import java.io.Serializable; // para los objetos convertilos en una secuencia de bytes (serializado)
 import java.util.ArrayList;
